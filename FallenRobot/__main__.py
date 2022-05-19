b49c929f59be6a0ff25220293c7d1e941e0b5f70
RubySpideybot <86848301+RubySpideybot@users.noreply.github.com>
2022-05-19 23:09:05 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -726,7 +726,7 @@ def main():
             updater.bot.set_webhook(url=URL + TOKEN)
 
     else:
-        LOGGER.info("Using long polling.")
+        LOGGER.info("Yoonie is Started.")
         updater.start_polling(timeout=15, read_latency=4, clean=True)
 
     if len(argv) not in (1, 3, 4):
