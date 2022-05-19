a7b5a7276330d52a613ac65a062e353e4fc4e9b1
RubySpideybot <86848301+RubySpideybot@users.noreply.github.com>
2022-05-20 00:12:26 +0530

    Update antiflood.py

--- FallenRobot/modules/antiflood.py
+++ FallenRobot/modules/antiflood.py
@@ -406,12 +406,12 @@ will result in restricting that user.
  This will mute users if they send more than 10 messages in a row, bots are ignored.
 
  ‣ /flood*:* Get the current flood control setting
-• *Admins only:*
+» *Admins only:*
  ‣ /setflood <int/'no'/'off'>*:* enables or disables flood control
  *Example:* `/setflood 10`
  ‣ /setfloodmode <ban/kick/mute/tban/tmute> <value>*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
-• *Note:*
- • Value must be filled for tban and tmute!!
+*» Note:*
+ » Value must be filled for tban and tmute!!
  It can be:
  `5m` = 5 minutes
  `6h` = 6 hours
