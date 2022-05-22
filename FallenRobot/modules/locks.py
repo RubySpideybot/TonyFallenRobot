1b2f293d6ba00fd0aeb5e10fe8b09f117a26daf4
RubySpideybot <86848301+RubySpideybot@users.noreply.github.com>
2022-05-22 19:02:37 +0530

    Update locks.py

--- FallenRobot/modules/locks.py
+++ FallenRobot/modules/locks.py
@@ -567,12 +567,12 @@ You're in the right place!
 The locks module allows you to lock away some common items in the \
 telegram world; the bot will automatically delete them!
 
- ❍ /locktypes*:* Lists all possible locktypes
+‣ /locktypes : Lists all possible locktypes
  
-*Admins only:*
- ❍ /lock <type>*:* Lock items of a certain type (not available in private)
- ❍ /unlock <type>*:* Unlock items of a certain type (not available in private)
- ❍ /locks*:* The current list of locks in this chat.
+*Admins only*:
+‣ /lock <type> : Lock items of a certain type (not available in private)
+‣ /unlock <type> : Unlock items of a certain type (not available in private)
+‣ /locks : The current list of locks in this chat.
  
 Locks can be used to restrict a group's users.
 eg:
@@ -580,12 +580,12 @@ Locking urls will auto-delete all messages with urls, locking stickers will rest
 non-admin users from sending stickers, etc.
 Locking bots will stop non-admins from adding bots to the chat.
 
-*Note:*
+*» Note:*
  • Unlocking permission *info* will allow members (non-admins) to change the group information, such as the description or the group name
  • Unlocking permission *pin* will allow members (non-admins) to pin a message in a group
 """
 
-__mod_name__ = "Lᴏᴄᴋs"
+__mod_name__ = "Locks"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes)
 LOCK_HANDLER = CommandHandler("lock", lock, pass_args=True)  # , filters=Filters.group)
