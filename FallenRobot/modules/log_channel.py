1f7984e6803544d7606970d0bc23a39e5b38fee1
RubySpideybot <86848301+RubySpideybot@users.noreply.github.com>
2022-05-22 19:04:02 +0530

    Update log_channel.py

--- FallenRobot/modules/log_channel.py
+++ FallenRobot/modules/log_channel.py
@@ -193,18 +193,18 @@ if is_module_loaded(FILENAME):
         return "No log channel is set for this group!"
 
     __help__ = """
-*Admins only:*
- ❍ /logchannel*:* get log channel info
- ❍ /setlog*:* set the log channel.
- ❍ /unsetlog*:* unset the log channel.
+*Admins only*:
+‣ /logchannel : get log channel info
+‣ /setlog : set the log channel.
+‣ /unsetlog : unset the log channel.
 
 Setting the log channel is done by:
-❍ adding the bot to the desired channel (as an admin!)
-❍ sending /setlog in the channel
-❍ forwarding the /setlog to the group
+‣ adding the bot to the desired channel (as an admin!)
+‣ sending /setlog in the channel
+‣ forwarding the /setlog to the group
 """
 
-    __mod_name__ = "Lᴏɢs​"
+    __mod_name__ = "Logs"
 
     LOG_HANDLER = CommandHandler("logchannel", logging)
     SET_LOG_HANDLER = CommandHandler("setlog", setlog)
